0208256e2d93c3ab25aae964b734ba8819b8f3c1
Bill Neubauer <wcn@google.com>
2020-06-19 13:43:08 -0700

    Avoid matching on the SISSL header.
    It's pretty generic apart from the key phrase "Sun Standards License"
    
    The classifier was triggering on the header reference to MPL 1.1, and the rest
    of the words just happen to match the SISSL header.

--- v2/scoring.go
+++ v2/scoring.go
@@ -135,7 +135,7 @@ func scoreDiffs(diffs []diffmatchpatch.Diff) int {
 			// these are words or phrases that appear in a single/small number of
 			// licenses. Can we leverage frequency analysis to identify these
 			// interesting words/phrases and auto-extract them?
-			for _, p := range []string{"autoconf exception", "class path exception", "gcc linking exception", "bison exception", "font exception", "imagemagick", "x consortium", "apache", "bsd", "affero"} {
+			for _, p := range []string{"autoconf exception", "class path exception", "gcc linking exception", "bison exception", "font exception", "imagemagick", "x consortium", "apache", "bsd", "affero", "sun standards"} {
 				if strings.Index(text, p) != -1 {
 					return introducedPhraseChange
 				}
